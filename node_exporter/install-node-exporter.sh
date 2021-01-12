26475fec5fa8b384c4683f47c606c18310344071
Nguyễn Ngọc Tài <nguyenngoctaibp@gmail.com>
2021-01-12 09:25:27 +0700

    add script install node exporter centos7

--- node_exporter/install-node-exporter.sh
+++
@@ -1,8 +0,0 @@
-useradd -M node_exporter
-
-usermod -L node_exporter
-
-
-chown -R node_exporter:node_exporter /etc/node_exporter
-
-chown -R node_exporter:node_exporter /etc/node_exporter/node_exporter
