bd482924b35d21e14d283f60a05ad030aa0cdd87
Nguyễn Ngọc Tài <nguyenngoctaibp@gmail.com>
2021-03-04 22:49:36 +0700

    Update install-grafana-centos7.sh

--- prometheus/install-grafana-centos7.sh
+++ prometheus/install-grafana-centos7.sh
@@ -1,1 +1,31 @@
 
+#!/bin/bash
+yum install -y wget
+
+cat << EOF > /etc/yum.repos.d/grafana.repo
+[grafana]
+name=grafana
+baseurl=https://packages.grafana.com/oss/rpm
+repo_gpgcheck=1
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.grafana.com/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+EOF
+
+sudo yum install grafana
+
+yum install fontconfig
+yum install freetype*
+yum install urw-fonts
+
+grafana-cli plugins install alexanderzobnin-zabbix-app
+
+firewall-cmd --zone=public --add-port=3000/tcp --permanent
+firewall-cmd --reload
+
+systemctl start grafana-server
+systemctl enable grafana-server.service
+
+
