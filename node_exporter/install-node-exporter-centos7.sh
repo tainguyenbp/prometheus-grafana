2952f6e245495750ed0db0ac1f7fe14ceae1fd1a
Nguyễn Ngọc Tài <nguyenngoctaibp@gmail.com>
2021-03-09 22:26:37 +0700

    Update install-node-exporter-centos7.sh

--- node_exporter/install-node-exporter-centos7.sh
+++ node_exporter/install-node-exporter-centos7.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+
+check_operating_system () {
+        [ -f /etc/redhat-release ] && awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release && return
+        [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
+        [ -f /etc/lsb-release ] && awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release && return
+}
+
+install_node_exporter_centos_7 () {
 yum install -y wget
 
 export version_node_exporter='1.1.1'
 export arch_node_exporter='linux-amd64'
-
 
 useradd --no-create-home --shell /bin/false node_exporter
 
@@ -49,3 +56,58 @@ systemctl start node_exporter.service
 systemctl status node_exporter.service
 
 rm -rf /etc/node_exporter.tar.gz
+
+}
+
+operating_system=`check_operating_system`
+
+if [ -f /etc/os-release ] 
+then
+     operating_system_version=`awk -F= '/^VERSION_CODENAME/{print $2}' /etc/os-release`
+fi
+
+if [[ $os_name == *"Ubuntu"* ]]; then
+    echo "======================================"
+    echo $os_name $os_version
+    if [[ "$os_version" = "bionic" ]]; then
+        url=$u18_url
+        update_debian
+    elif [[ $os_version = "xenial" ]]; then
+        url=$u16_url
+        update_debian
+    elif [[ $os_version = "focal" ]]; then
+        url=$u20_url
+        update_debian
+    else
+        echo "$os_verion khong duoc support"
+    fi
+elif [[ $os_name == *"CentOS"* ]]; then
+    echo "======================================"
+    echo $os_name
+    if [[ $os_name == *"CentOS 6"* ]]; then
+        install_node_exporter_centos_6
+    elif [[ $os_name == *"CentOS 7"* ]]; then
+        install_node_exporter_centos_7
+    elif [[ $os_name == *"CentOS 8"* ]]; then
+        install_node_exporter_centos_8
+    else
+        echo "$os_name $os_version khong duoc support"
+    fi
+elif [[ $os_name == *"Debian"* ]]; then
+    echo "======================================"
+    echo $os_name $os_version
+    dpkg -l | grep sudo
+    echo "======================================"
+    if [[ $os_version = "stretch" ]]; then
+        url=$deb9_url
+        update_debian
+    elif [[ $os_version = "buster" ]]; then
+        url=$deb10_url
+        update_debian
+    else
+        echo "$os_verion khong duoc support"
+    fi
+else
+   echo "$os_name $os_verion  khong duoc support"
+fi
+
